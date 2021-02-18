e045d9eb121fbbb5c006ff69166de2205cef67e6
lewns2 <lewns2@naver.com>
2021-02-18 17:40:00 +0900

    '.'

---
+++ FIR_vivado/project_1.srcs/sources_1/bd/design_1/ip/design_1_fir_compiler_0_0/design_1_fir_compiler_0_0_stub.vhdl
@@ -0,0 +1,38 @@
+-- Copyright 1986-2018 Xilinx, Inc. All Rights Reserved.
+-- --------------------------------------------------------------------------------
+-- Tool Version: Vivado v.2018.3 (win64) Build 2405991 Thu Dec  6 23:38:27 MST 2018
+-- Date        : Mon Oct 12 05:28:22 2020
+-- Host        : LAPTOP-BC8BPLF4 running 64-bit major release  (build 9200)
+-- Command     : write_vhdl -force -mode synth_stub
+--               c:/Users/0/Desktop/1009/project_1/project_1.srcs/sources_1/bd/design_1/ip/design_1_fir_compiler_0_0/design_1_fir_compiler_0_0_stub.vhdl
+-- Design      : design_1_fir_compiler_0_0
+-- Purpose     : Stub declaration of top-level module interface
+-- Device      : xc7z020clg400-1
+-- --------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity design_1_fir_compiler_0_0 is
+  Port ( 
+    aclk : in STD_LOGIC;
+    s_axis_data_tvalid : in STD_LOGIC;
+    s_axis_data_tready : out STD_LOGIC;
+    s_axis_data_tlast : in STD_LOGIC;
+    s_axis_data_tdata : in STD_LOGIC_VECTOR ( 31 downto 0 );
+    m_axis_data_tvalid : out STD_LOGIC;
+    m_axis_data_tready : in STD_LOGIC;
+    m_axis_data_tlast : out STD_LOGIC;
+    m_axis_data_tdata : out STD_LOGIC_VECTOR ( 31 downto 0 )
+  );
+
+end design_1_fir_compiler_0_0;
+
+architecture stub of design_1_fir_compiler_0_0 is
+attribute syn_black_box : boolean;
+attribute black_box_pad_pin : string;
+attribute syn_black_box of stub : architecture is true;
+attribute black_box_pad_pin of stub : architecture is "aclk,s_axis_data_tvalid,s_axis_data_tready,s_axis_data_tlast,s_axis_data_tdata[31:0],m_axis_data_tvalid,m_axis_data_tready,m_axis_data_tlast,m_axis_data_tdata[31:0]";
+attribute x_core_info : string;
+attribute x_core_info of stub : architecture is "fir_compiler_v7_2_11,Vivado 2018.3";
+begin
+end;
---
+++ README.md
@@ -0,0 +1,1 @@
+"# Condition-Monitoring-System-with-PYNQ" 
